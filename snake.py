440dc7fd46e738619d8823b1066de639d564142f
MOMOTA66 <1103192112ka@gmail.com>
2023-09-02 18:38:07 +0800

    修正 邊界判斷

--- snake.py
+++ snake.py
@@ -22,18 +22,16 @@ currentDirection = "right"  # 當前方向 右移
 
 # 畫面渲染
 def main():
-    global snake_X, snake_Y, currentDirection
-
     while True:
         for i in range(maxLength):
             for j in range(maxLength):
                 if i in (0, maxLength - 1) or j in (0, maxLength - 1):
                     print(wall, end="")
                 # 打印 蛇的畫面
-                elif i == snake_X and j == snake_Y:
+                elif i == snake_Y and j == snake_X:
                     print(snake_symbol, end="")
                 #打印 星星的畫面
-                elif i == star_X and j == star_Y:
+                elif i == star_Y and j == star_X:
                     print(star_symbol, end="")
                 else:
                     print(empty, end=" ")
@@ -41,7 +39,7 @@ def main():
             print()
 
         snakeState()  # 更新 蛇 的狀態
-        starState     # 更新 星 的狀態
+        # starState     # 更新 星 的狀態
         # 設定畫面更新時間
         time.sleep(timeLength)
         # 清除畫面
@@ -51,8 +49,10 @@ def main():
 
 # 蛇的狀態
 def snakeState():
+    
     global snake_X, snake_Y, currentDirection
-    new_X, new_Y = snake_X, snake_Y
+    new_X = snake_X
+    new_Y = snake_Y
 
     if keyboard.is_pressed("right") and currentDirection != "left":
         currentDirection = "right"
@@ -64,32 +64,32 @@ def snakeState():
         currentDirection = "down"
 
     if currentDirection == "right":
-        snake_Y = min(maxLength - 2, max(1, new_Y + 1))
+        new_X = new_X + 1
     elif currentDirection == "left":
-        snake_Y = min(maxLength - 2, max(1, new_Y - 1))
+        new_X = new_X - 1
     elif currentDirection == "up":
-        snake_X = min(maxLength - 2, max(1, new_X - 1))
+        new_Y = new_Y - 1
     elif currentDirection == "down":
-        snake_X = min(maxLength - 2, max(1, new_X + 1))
+        new_Y = new_Y + 1
+
+    # 判斷站存的x和y是不是在牆壁上，如果是的話就不要用佔存的值
+    isTouchWall = new_X == 0 or new_X == maxLength - 1 or new_Y == 0 or new_Y == maxLength - 1
+    if not isTouchWall:
+         snake_X = new_X
+         snake_Y = new_Y
+    
 
 
 
-def starState():
-    global star_X, star_Y,currentSnakeCoordinate
-    new_X, new_Y = star_X, star_Y
 
-    if currentDirection == "right":
-        snake_Y = min(maxLength - 2, max(1, new_Y ))
-    elif currentDirection == "left":
-        snake_Y = min(maxLength - 2, max(1, new_Y ))
-    elif currentDirection == "up":
-        snake_X = min(maxLength - 2, max(1, new_X ))
-    elif currentDirection == "down":
-        snake_X = min(maxLength - 2, max(1, new_X ))
+
+# def starState():
+#     global star_X, star_Y,currentSnakeCoordinate
+#     new_X, new_Y = star_X, star_Y
 
 
 
-if __name__ == "__main__":
-    main()
+main()
+# if __name__ == "__main__":
 
 
