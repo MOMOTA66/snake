f2a45722459c23da502b6c0e7624a516d352c60c
MOMOTA66 <1103192112ka@gmail.com>
2023-09-03 13:41:47 +0800

    星星出現在蛇附近 短時間內重複吃到出現BUG

--- snake.py
+++ snake.py
@@ -4,21 +4,22 @@ import keyboard
 import random
 
 # 蛇初始位置
-snake_X = 5
-snake_Y = 5
+snake_X = 2
+snake_Y = 2
 
-star_X = 8
-star_Y = 8
+star_X = 3
+star_Y = 3
 
 wall = " 牆"
 empty = " -"
 snake_symbol = " 蛇"
-star_symbol =" 星"
-maxLength = 15
+snake_Lenght = 1
+star_symbol = " 星"
+maxLength = 10
 timeLength = 0.1
 currentDirection = "right"  # 當前方向 右移
-
-
+snake_recordPath = []
+last_recorded_path = None 
 
 # 畫面渲染
 def main():
@@ -30,40 +31,43 @@ def main():
                 # 打印 蛇的畫面
                 elif i == snake_Y and j == snake_X:
                     print(snake_symbol, end="")
-                #打印 星星的畫面
+                # 打印 星星的畫面
                 elif i == star_Y and j == star_X:
                     print(star_symbol, end="")
                 else:
                     print(empty, end=" ")
             # 打印畫面
             print()
-
-        updateSnakeState()  # 更新 蛇 的狀態
+        
+        # 記錄 蛇 的路徑
+        recordSnakePath()
+        # 更新 蛇 的狀態
+        updateSnakeState()  
         if isEatStar():
+            # 增加 蛇 的長度
+            add_snake_Length_()
+            # 更新 星 的位置
             randomStarPosition()
 
-        # starState     # 更新 星 的狀態
+        print(snake_Lenght)
         # 設定畫面更新時間
         time.sleep(timeLength)
         # 清除畫面
         os.system("cls" if os.name == "nt" else "clear")
 
-
-
 # 蛇的狀態
 def updateSnakeState():
-    
     global snake_X, snake_Y, currentDirection
     new_X = snake_X
     new_Y = snake_Y
 
-    if keyboard.is_pressed("right") and currentDirection != "left":
+    if keyboard.is_pressed("right"):  # and currentDirection != "left":
         currentDirection = "right"
-    elif keyboard.is_pressed("left") and currentDirection != "right":
+    elif keyboard.is_pressed("left"):  # and currentDirection != "right":
         currentDirection = "left"
-    elif keyboard.is_pressed("up") and currentDirection != "down":
+    elif keyboard.is_pressed("up"):  # and currentDirection != "down":
         currentDirection = "up"
-    elif keyboard.is_pressed("down") and currentDirection != "up":
+    elif keyboard.is_pressed("down"):  # and currentDirection != "up":
         currentDirection = "down"
 
     if currentDirection == "right":
@@ -78,26 +82,43 @@ def updateSnakeState():
     # 判斷站存的x和y是不是在牆壁上，如果是的話就不要用佔存的值
     isTouchWall = new_X == 0 or new_X == maxLength - 1 or new_Y == 0 or new_Y == maxLength - 1
     if not isTouchWall:
-         snake_X = new_X
-         snake_Y = new_Y
-    
+        snake_X = new_X
+        snake_Y = new_Y
+
 def isEatStar():
-    return  snake_X == star_X and snake_Y ==star_Y
-        
+    return  snake_X == star_X and snake_Y == star_Y 
+
 def randomStarPosition():
-    star_X=random.randint(1, maxLength - 1 - 1)
-    star_Y=random.randint(1, maxLength - 1 - 1)
+    max_attempts = 100  # 最多嘗試100次
+    attempts = 0
+
+    while attempts < max_attempts:
+        global star_X, star_Y
+
+        random_X = random.randint(1, maxLength - 1 - 1)
+        random_Y = random.randint(1, maxLength - 1 - 1)
+
+        # 判斷不重疊 就產生新的星星
+        isNotCover = (random_X, random_Y) not in snake_recordPath
+        if isNotCover:
+            star_X = random_X
+            star_Y = random_Y
+            break
+
+def recordSnakePath():
+    global last_recorded_path
+    snake_Path = (snake_X, snake_Y)
     
+    if snake_Path != last_recorded_path:
+        snake_recordPath.insert(0, snake_Path)
+        last_recorded_path = snake_Path
 
-
-
-def starState():
-     global star_X, star_Y,currentSnakeCoordinate
-     new_X, new_Y = star_X, star_Y
-
-
+def add_snake_Length_():
+    global snake_Lenght 
+    snake_Lenght = snake_Lenght+1   
 
 main()
-# if __name__ == "__main__":
+
+
 
 
